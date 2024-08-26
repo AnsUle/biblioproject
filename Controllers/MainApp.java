35034c3952ef2c65c332883b5aa91bc56f68e191
AnsUle <anna.suleyman@yahoo.fr>
2024-08-26 16:34:05 +0200

    Merge remote-tracking branch 'origin/main'

--- Controllers/MainApp.java
+++
@@ -1,9 +0,0 @@
-package Controllers;
-
-public class MainApp {
-    public static void main(String[] args) {
-        //Bibliotheque bibliotheque = new Bibliotheque();
-        Object MenuFrameSwing;
-
-    }
-}
